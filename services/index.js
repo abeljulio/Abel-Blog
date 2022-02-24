fa8b8bdb894a09f618115436998de5b6565dad22
Abel Julio <acelina101@gmail.com>
2022-02-24 11:18:33 +0700

    Fixing bug in category page

--- services/index.js
+++ services/index.js
@@ -161,7 +161,7 @@ export const getComments = async (slug) => {
 
 export const getFeaturedPosts = async () => {
   const query = gql`
-    query GetCategoryPost {
+    query GetFeaturedPosts {
       posts(where: { featuredPost: true }) {
         author {
           name
@@ -182,4 +182,41 @@ export const getFeaturedPosts = async () => {
   const result = await request(graphqlAPI, query)
 
   return result.posts
+}
+
+export const getCategoryPost = async (slug) => {
+  const query = gql`
+    query GetCategoryPost($slug: String!) {
+      postsConnection(where: { categories_some: { slug: $slug } }) {
+        edges {
+          cursor
+          node {
+            author {
+              bio
+              name
+              id
+              photo {
+                url
+              }
+            }
+            createdAt
+            slug
+            title
+            excerpt
+            featuredImage {
+              url
+            }
+            categories {
+              name
+              slug
+            }
+          }
+        }
+      }
+    }
+  `
+
+  const result = await request(graphqlAPI, query, { slug })
+
+  return result.postsConnection.edges
 }
